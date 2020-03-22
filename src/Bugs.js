bbaf1c1fdb2caef82d161226a8e0c825585c0134
Brandon Black <brandonblack02@gmail.com>
2020-03-22 14:40:49 -0700

    Bug and Fish mapping complete

--- src/Bugs.js
+++ src/Bugs.js
@@ -2,9 +2,14 @@ import React from 'react';
 import bugs from './bugs.json';
 
 export default function Bugs() {
+
+    let bugEntries = Object.entries(bugs);
+    let mappedBugs = bugEntries.map( (bug, id) => <div className="bug" key={id}>{bug[0]} <p className="prices">{bug[1]}</p> </div>);
+
     return (
         <div className="bugs">
-            <h1>Bugs</h1>
+            <h1>Bugs:</h1>
+            {mappedBugs}
         </div>
     )
 }
--- src/Fish.js
+++ src/Fish.js
@@ -4,8 +4,9 @@ import fish from './fish.json';
 export default function Fish() {
     let fishEntries = Object.entries(fish);
     let mappedFish = fishEntries.map( (fish, id) => 
-        <div className="fish" key={id}>{fish[0]} <p>{fish[1]}</p></div>
+        <div className="fish" key={id}>{fish[0]} <p className="prices">{fish[1]}</p></div>
     );
+
     return (
         <div className="fishes">
             <h1>Fish:</h1>
