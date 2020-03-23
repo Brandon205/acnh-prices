3290e4ad250a9f23f7c5058b689238d6e37022a7
brandonblack <brandonblack02@gmail.com>
2020-03-22 21:54:57 -0700

    Updates

---
+++ precache-manifest.63c2ba797dce34eacfa3e1ae60ea88a8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "88f1c2755dcfc7f5f91bc0041a96a001",
+    "url": "/acnh-prices/index.html"
+  },
+  {
+    "revision": "bb3550cf851527e27038",
+    "url": "/acnh-prices/static/css/main.fb5f0add.chunk.css"
+  },
+  {
+    "revision": "555760ceb7319d39cad3",
+    "url": "/acnh-prices/static/js/2.e9a50208.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/acnh-prices/static/js/2.e9a50208.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bb3550cf851527e27038",
+    "url": "/acnh-prices/static/js/main.ad4072f5.chunk.js"
+  },
+  {
+    "revision": "8b351df2052d76b8d84a",
+    "url": "/acnh-prices/static/js/runtime-main.1f7a7b01.js"
+  }
+]);
