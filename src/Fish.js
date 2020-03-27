40eb22c2b4c98ce2cc5d69a2da3a9b0d6e609149
Brandon Black <brandonblack02@gmail.com>
2020-03-27 13:03:25 -0700

    Modal made for Fish.js

--- src/Fish.js
+++ src/Fish.js
@@ -1,12 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 export default function Fish(props) {
-    let mappedFish = props.fish.map( (fish, id) => <div className="animal" key={id}>{fish[0]} <p className="prices">{fish[1].price}</p></div> );
+    const [modal, setModal] = useState('');
+    
+    let mappedFish = props.fish.map( (fish, id) => <div className="animal" key={id} onClick={() => expand(fish)}>{fish[0]} <p className="prices">{fish[1].price}</p></div> );
 
+    let expand = (fish) => {
+        var formatter = new Intl.NumberFormat();
+        let content = (
+        <div className="modal" onClick={() => setModal('')}>
+                <div className="modal-content">
+                    <h1>{fish[0]}</h1>
+                    <hr />
+                    <h3>Swims around the {fish[1].location}</h3>
+                    <h3>Sells for {formatter.format(fish[1].price)} bells</h3>
+                    <h3>{fish[1].time}</h3>
+                    <h3>{fish[1].months}</h3>
+                    <i>N - Northern Hemishpere, S - Southern Hemishpere</i>
+                </div>
+            </div>
+        )
+        setModal(content);
+    }
+    
     return (
         <div className="price-list">
             <h1>ACNH Fish:</h1>
             {mappedFish}
+            {modal}
         </div>
     )
 }
