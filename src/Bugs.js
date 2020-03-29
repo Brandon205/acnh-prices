e0594da0ffa751e703e68b9c64b146c4a2c37a39
brandonblack <brandonblack02@gmail.com>
2020-03-28 17:30:02 -0700

    Added Bug images

--- src/Bugs.js
+++ src/Bugs.js
@@ -10,6 +10,7 @@ export default function Bugs(props) {
         let content = (
             <div className="modal" onClick={() => setModal('')}>
                 <div className="modal-content">
+                    <img src={process.env.PUBLIC_URL + '/images/' + bug[0] + '.png'} alt={bug[0]} />
                     <h1>{bug[0]}</h1>
                     <hr />
                     <h3>{bug[1].location}</h3>
