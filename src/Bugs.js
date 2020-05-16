f476a6932f23b026f3e84b25cee74e335c9d17eb
brandonblack <brandonblack02@gmail.com>
2020-05-15 19:16:59 -0700

    Bug.js Layout Changes

--- src/Bugs.js
+++ src/Bugs.js
@@ -4,7 +4,17 @@ export default function Bugs(props) {
     const [modal, setModal] = useState('');
     var formatter = new Intl.NumberFormat();
 
-    let mappedBugs = props.bugs.map( (bug, id) => <div className="animal" key={id} onClick={() => expand(bug)}>{bug.name} <p className="prices">{formatter.format(bug.price)}<img className="bells-img" src={process.env.PUBLIC_URL + "/images/BellIcon.png"} alt="bell icon" /></p></div>);
+    let mappedBugs = props.bugs.map( (bug, id) => 
+    <div className="animal" key={id} onClick={() => expand(bug)}>
+        <>
+            <p className="animal-name">{bug.name}</p> 
+        </>
+        <hr className="modal-hr" />
+        <>
+            <p className="prices">{bug.location}</p>
+            <p className="prices">{formatter.format(bug.price)}<img className="bells-img" src={process.env.PUBLIC_URL + "/images/BellIcon.png"} alt="bell icon" /></p>
+        </>
+        </div>);
 
     let expand = (bug) => {
         let content = (
@@ -12,7 +22,7 @@ export default function Bugs(props) {
                 <div className="modal-content">
                     <img src={process.env.PUBLIC_URL + '/images/' + bug.name + '.png'} alt={bug[0]} />
                     <h1>{bug.name}</h1>
-                    <hr />
+                    <hr className="modal-hr" />
                     <h3>{bug.location}</h3>
                     <h3>Sells for {formatter.format(bug.price)} bells</h3>
                     <h3>{bug.time}</h3>
