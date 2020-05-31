b26c0d651bc612870ad9838cde063073a656955a
Brandon Black <brandonblack02@gmail.com>
2020-05-31 08:46:55 -0700

    Fixed component updating issue

--- src/Display.js
+++ src/Display.js
@@ -1,8 +1,13 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 export default function Display(props) {
     const [modal, setModal] = useState('');
+    const [display, setDisplay] = useState([]);
     var formatter = new Intl.NumberFormat();
+
+    useEffect( () => {
+        setDisplay(props.list)
+    }, [props.list]);
 
     let setFavorite = (name) => {
         let currStorage;
@@ -19,10 +24,14 @@ export default function Display(props) {
         let ls = localStorage.getItem('animals').split(',');
         ls.splice(ls.indexOf(name), 1);
         localStorage.setItem('animals', ls);
-        props.setAnimals(ls);
+        for (let i = 0; i < display.length; i++) {
+            if (display[i].name === name) {
+                display.splice(i, 1);
+            }
+        }
     }
 
-    let mappedAnimals = props.list.map( (animal, id) => 
+    let mappedAnimals = display.map( (animal, id) => 
     <div className="animal" key={id} onClick={() => expand(animal)}>
         <>
             <p className="animal-name">{animal.name}</p>
@@ -41,7 +50,7 @@ export default function Display(props) {
                     <img src={process.env.PUBLIC_URL + '/images/' + animal.name + '.png'} alt={animal[0]}/>
                     <h1>{animal.name}</h1>
                     {props.all ? <button className="favorite" onClick={() => setFavorite(animal.name)}>+ Add to Your List</button> : ''}
-                    {props.yours ? <button className="favorite" onClick={() => removeFavorite(animal.name)}>- Remove from Your List</button> : ''}
+                    {props.yours ? <button className="favorite" onClick={() => { removeFavorite(animal.name) }}>- Remove from Your List</button> : ''}
                     <hr className="modal-hr" />
                     <h3>{animal.location}</h3>
                     <h3>Sells for {formatter.format(animal.price)} bells</h3>
--- src/YourList.js
+++ src/YourList.js
@@ -1,20 +1,19 @@
-import React, { useState } from 'react';
+import React from 'react';
 import Display from './Display';
 import allPrices from './assets/allAnimals.json';
 
 export default function YourList() {
-    const [animals, setAnimals] = useState('');
 
-    let content;
+    let content = '';
     let displayArr = [];
     if (localStorage.getItem('animals') === null || localStorage.getItem('animals').length === 0) { // If there are favorited animals then it should loop through them
         content = (<><h1>You haven't added any animals to your list!</h1><p>To do that all you need to do is hit the + sign on the animals info panel.</p></>)
-    } else { // For each animal in LS it should check to see if thats an animal in the JSON file, if so add all that animals info to the displayArr to be displayed later
+    } else { // For each animal in LS it should check to see if thats an animal in the JSON file, if so add all that animals info to the display array to be displayed later
         let ls = localStorage.getItem('animals').split(',');
         ls.forEach(animal => {
             allPrices.some(item => {
                 if (item.name === animal) {
-                    displayArr.push(item);
+                    displayArr.push(item)
                     return true;
                 }
                 return false;
@@ -25,7 +24,7 @@ export default function YourList() {
     return (
         <div className="price-list">
             {content}
-            <Display list={displayArr} yours={true} setAnimals={() => setAnimals()} animals={animals} />
+            <Display list={displayArr} yours={true} />
         </div>
     )
 }
